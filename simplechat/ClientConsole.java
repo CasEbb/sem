7695002065d9f8b9f06c7ea85a671d5c2707a2f9
Cas Ebbers <cjaebbers@gmail.com>
2013-11-15 09:46:58 +0100

    Code formatted

--- simplechat/ClientConsole.java
+++ simplechat/ClientConsole.java
@@ -9,123 +9,107 @@ import simplechat.client.*;
 import simplechat.common.*;
 
 /**
- * This class constructs the UI for a chat client.  It implements the
- * chat interface in order to activate the display() method.
- * Warning: Some of the code here is cloned in ServerConsole
- *
+ * This class constructs the UI for a chat client. It implements the chat
+ * interface in order to activate the display() method. Warning: Some of the
+ * code here is cloned in ServerConsole
+ * 
  * @author Fran&ccedil;ois B&eacute;langer
  * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;re
  * @version July 2000
  */
-public class ClientConsole implements ChatIF
-{
-  //Class variables *************************************************
+public class ClientConsole implements ChatIF {
+	// Class variables *************************************************
 
-  /**
-   * The default port to connect on.
-   */
-  final public static int DEFAULT_PORT = 5555;
+	/**
+	 * The default port to connect on.
+	 */
+	final public static int DEFAULT_PORT = 5555;
 
-  //Instance variables **********************************************
+	// Instance variables **********************************************
 
-  /**
-   * The instance of the client that created this ConsoleChat.
-   */
-  ChatClient client;
+	/**
+	 * The instance of the client that created this ConsoleChat.
+	 */
+	ChatClient client;
 
+	// Constructors ****************************************************
 
-  //Constructors ****************************************************
+	/**
+	 * Constructs an instance of the ClientConsole UI.
+	 * 
+	 * @param host
+	 *            The host to connect to.
+	 * @param port
+	 *            The port to connect on.
+	 */
+	public ClientConsole(String host, int port) {
+		try {
+			client = new ChatClient(host, port, this);
+		} catch (IOException exception) {
+			System.out.println("Error: Can't setup connection!"
+					+ " Terminating client.");
+			System.exit(1);
+		}
+	}
 
-  /**
-   * Constructs an instance of the ClientConsole UI.
-   *
-   * @param host The host to connect to.
-   * @param port The port to connect on.
-   */
-  public ClientConsole(String host, int port)
-  {
-    try
-    {
-      client= new ChatClient(host, port, this);
-    }
-    catch(IOException exception)
-    {
-      System.out.println("Error: Can't setup connection!"
-                + " Terminating client.");
-      System.exit(1);
-    }
-  }
+	// Instance methods ************************************************
 
+	/**
+	 * This method waits for input from the console. Once it is received, it
+	 * sends it to the client's message handler.
+	 */
+	public void accept() {
+		try {
+			BufferedReader fromConsole = new BufferedReader(
+					new InputStreamReader(System.in));
+			String message;
 
-  //Instance methods ************************************************
+			while (true) {
+				message = fromConsole.readLine();
+				client.handleMessageFromClientUI(message);
+			}
+		} catch (Exception ex) {
+			System.out.println("Unexpected error while reading from console!");
+		}
+	}
 
-  /**
-   * This method waits for input from the console.  Once it is
-   * received, it sends it to the client's message handler.
-   */
-  public void accept()
-  {
-    try
-    {
-      BufferedReader fromConsole =
-        new BufferedReader(new InputStreamReader(System.in));
-      String message;
+	/**
+	 * This method overrides the method in the ChatIF interface. It displays a
+	 * message onto the screen.
+	 * 
+	 * @param message
+	 *            The string to be displayed.
+	 */
+	public void display(String message) {
+		System.out.println("> " + message);
+	}
 
-      while (true)
-      {
-        message = fromConsole.readLine();
-        client.handleMessageFromClientUI(message);
-      }
-    }
-    catch (Exception ex)
-    {
-      System.out.println
-        ("Unexpected error while reading from console!");
-    }
-  }
+	// Class methods ***************************************************
 
-  /**
-   * This method overrides the method in the ChatIF interface.  It
-   * displays a message onto the screen.
-   *
-   * @param message The string to be displayed.
-   */
-  public void display(String message)
-  {
-    System.out.println("> " + message);
-  }
+	/**
+	 * This method is responsible for the creation of the Client UI.
+	 * 
+	 * @param args
+	 *            [0] The host to connect to.
+	 */
+	public static void main(String[] args) {
+		String host = "";
+		int port = 0; // The port number
 
-
-  //Class methods ***************************************************
-
-  /**
-   * This method is responsible for the creation of the Client UI.
-   *
-   * @param args[0] The host to connect to.
-   */
-  public static void main(String[] args)
-  {
-    String host = "";
-    int port = 0;  //The port number
-
-    try
-    {
-      host = args[0];
-    }
-    catch(ArrayIndexOutOfBoundsException e)
-    {
-      host = "localhost";
-    }
-    // 1.2.2
-    try {
-    	port = Integer.parseInt(args[1]);
-    }
-    catch(ArrayIndexOutOfBoundsException e){
-    	port = DEFAULT_PORT;
-    }
-    ClientConsole chat = new ClientConsole(host, port);
-    chat.accept();  //Wait for console data
-  }
+		try {
+			host = args[0];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			host = "localhost";
+		}
+		// 1.2.2
+		try {
+			port = Integer.parseInt(args[1]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			port = DEFAULT_PORT;
+		}
+		ClientConsole chat = new ClientConsole(host, port);
+		chat.accept(); // Wait for console data
+	}
 }
-//End of ConsoleChat class
+// End of ConsoleChat class
--- simplechat/client/ChatClient.java
+++ simplechat/client/ChatClient.java
@@ -9,95 +9,88 @@ import simplechat.common.*;
 import java.io.*;
 
 /**
- * This class overrides some of the methods defined in the abstract
- * superclass in order to give more functionality to the client.
- *
+ * This class overrides some of the methods defined in the abstract superclass
+ * in order to give more functionality to the client.
+ * 
  * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;
  * @author Fran&ccedil;ois B&eacute;langer
  * @version July 2000
  */
-public class ChatClient extends AbstractClient
-{
-  //Instance variables **********************************************
+public class ChatClient extends AbstractClient {
+	// Instance variables **********************************************
 
-  /**
-   * The interface type variable.  It allows the implementation of
-   * the display method in the client.
-   */
-  ChatIF clientUI;
+	/**
+	 * The interface type variable. It allows the implementation of the display
+	 * method in the client.
+	 */
+	ChatIF clientUI;
 
+	// Constructors ****************************************************
 
-  //Constructors ****************************************************
+	/**
+	 * Constructs an instance of the chat client.
+	 * 
+	 * @param host
+	 *            The server to connect to.
+	 * @param port
+	 *            The port number to connect on.
+	 * @param clientUI
+	 *            The interface type variable.
+	 */
 
-  /**
-   * Constructs an instance of the chat client.
-   *
-   * @param host The server to connect to.
-   * @param port The port number to connect on.
-   * @param clientUI The interface type variable.
-   */
+	public ChatClient(String host, int port, ChatIF clientUI)
+			throws IOException {
+		super(host, port); // Call the superclass constructor
+		this.clientUI = clientUI;
+		openConnection();
+	}
 
-  public ChatClient(String host, int port, ChatIF clientUI)
-    throws IOException
-  {
-    super(host, port); //Call the superclass constructor
-    this.clientUI = clientUI;
-    openConnection();
-  }
+	// Instance methods ************************************************
 
+	/**
+	 * This method handles all data that comes in from the server.
+	 * 
+	 * @param msg
+	 *            The message from the server.
+	 */
+	public void handleMessageFromServer(Object msg) {
+		clientUI.display(msg.toString());
+	}
 
-  //Instance methods ************************************************
+	/**
+	 * This method handles all data coming from the UI
+	 * 
+	 * @param message
+	 *            The message from the UI.
+	 */
+	public void handleMessageFromClientUI(String message) {
+		try {
+			sendToServer(message);
+		} catch (IOException e) {
+			clientUI.display("Could not send message to server.  Terminating client.");
+			quit();
+		}
+	}
 
-  /**
-   * This method handles all data that comes in from the server.
-   *
-   * @param msg The message from the server.
-   */
-  public void handleMessageFromServer(Object msg)
-  {
-    clientUI.display(msg.toString());
-  }
+	// 1.2.1
+	public void connectionClosed() {
+		System.out.println("Connection closed.");
+	}
 
-  /**
-   * This method handles all data coming from the UI
-   *
-   * @param message The message from the UI.
-   */
-  public void handleMessageFromClientUI(String message)
-  {
-    try
-    {
-      sendToServer(message);
-    }
-    catch(IOException e)
-    {
-      clientUI.display
-        ("Could not send message to server.  Terminating client.");
-      quit();
-    }
-  }
-  
-  // 1.2.1
-  public void connectionClosed(){
-	  System.out.println("Connection closed.");
-  }
-  
-  public void connectionException(Exception e){
-	  quit();
-  }
+	public void connectionException(Exception e) {
+		quit();
+	}
 
-  /**
-   * This method terminates the client.
-   */
-  public void quit()
-  {
-    try
-    {
-      closeConnection();
-    }
-    catch(IOException e) {}
-    System.exit(0);
-  }
+	/**
+	 * This method terminates the client.
+	 */
+	public void quit() {
+		try {
+			closeConnection();
+		} catch (IOException e) {
+		}
+		System.exit(0);
+	}
 }
-//End of ChatClient class
+// End of ChatClient class
