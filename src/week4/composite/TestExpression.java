d4dc443301a500ba508e02a7f1b76d94c64108e3
Cas Ebbers <cjaebbers@gmail.com>
2013-12-03 12:12:51 +0100

    Singletonned test class

--- src/week4/composite/TestExpression.java
+++ src/week4/composite/TestExpression.java
@@ -1,10 +1,30 @@
 package week4.composite;
 
 public class TestExpression {
-	public static void main(String[] args) {
+
+	private static TestExpression instance;
+
+	private TestExpression() {
+	}
+
+	public static TestExpression getInstance() {
+		if (instance == null) {
+			instance = new TestExpression();
+		}
+
+		return instance;
+	}
+
+	public int getTestResult() {
 		MinExpression exp1 = new MinExpression(new Number(5), new Number(7));
 		ProdExpression exp2 = new ProdExpression(exp1, new Number(8));
 		PlusExpression exp3 = new PlusExpression(exp2, new Number(4));
-		System.out.println(exp3.result());
+		return exp3.result();
 	}
+
+	public static void main(String[] args) {
+		TestExpression test = TestExpression.getInstance();
+		System.out.println(test.getTestResult());
+	}
+
 }
